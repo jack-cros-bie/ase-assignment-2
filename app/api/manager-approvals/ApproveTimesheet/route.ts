d5d6f09f7ac09cd04e54c103ef6730de0d5937e1
TB-UNI-DEV <thomas.barnett.237@cranfield.ac.uk>
2025-07-17 14:23:35 +0100

    SCRUM-54 add ability to approve or reject timesheet

---
+++ app/api/manager-approvals/ApproveTimesheet/route.ts
@@ -0,0 +1,36 @@
+// File: /api/manager-approvals/SetAnnualLeaveApproval/route.ts
+import { NextResponse } from 'next/server';
+import { query } from '@/server/sql/sqlHandler.server';
+
+export async function POST(request: Request) {
+    try {
+    // Extract the managerId from the query parameters.
+    const urlParams = new URLSearchParams(request.url);
+    const timesheetentryid = urlParams.get('leaveentryid');
+
+    if (!timesheetentryid) {
+      return NextResponse.json({ error: 'timesheetentryid parameter is required' }, { status: 400 });
+    }
+
+    const parsedTimesheetEntryId = parseInt(timesheetentryid, 10);
+
+    if (isNaN(parsedTimesheetEntryId)) {
+      return NextResponse.json({ error: 'Invalid timesheetentryid.  Must be a number.' }, { status: 400 });
+    }
+
+    const TimesheetRecord = await query<{}>(
+      `UPDATE timesheets
+       SET approved = TRUE
+       WHERE timesheetentryid = $1`,
+       // Use parameterized query
+      [parsedTimesheetEntryId] // Pass the timesheetentryid as a parameter
+    );
+
+    console.log(TimesheetRecord)
+
+    return NextResponse.json(TimesheetRecord);
+  } catch (error) {
+    console.error('Error setting timesheet approval:', error);
+    return NextResponse.json({ error: 'Failed to approve timesheet'}, { status: 500 });
+  }
+}
